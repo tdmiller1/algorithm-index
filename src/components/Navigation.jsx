b8fb7a2b929dd3c6662d299a900bc0940aacdc00
Tucker Miller <tuckerdanielmiller@gmail.com>
2021-01-13 20:26:42 -0500

    Add navigation sidebar

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,31 +1,64 @@
-import React, { useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import {
   Link
 } from "react-router-dom";
+import { Button, Grid } from '@material-ui/core';
 
 import pageJson from './pageContent.json';
+import NavigationLink from './NavigationLink';
 
 const Navigation = () => {
   const [pages] = useState(pageJson.pages);
+  const [components, setComponents] = useState([]);
+  const [pageStuff, setPageStuff] = useState(null);
+
+  const hasPages = page => !!page.pages
+
+  const printPageTitle = useCallback(page => {
+    if(!page) return null
+
+    components.push(page);
+    setComponents(components);
+
+    if(hasPages(page)) {
+      page.pages.forEach(page => {
+        printPageTitle(page);
+      })
+    }
+  }, [components] );
+
+  useEffect(() => {
+    if (components.length === 0) {
+      pages.forEach(page => {
+        if(hasPages(page)) {
+          printPageTitle(page);
+        }
+      })
+    }
+
+    setPageStuff(components)
+  }, [pages, printPageTitle, components]);
 
   return (
     <div className="bg-gray-100">
       <nav>
         <ul>
-          <Link to="/">
-            <li className="pl-3 pr-3 py-3">
-              Home
-            </li>
-          </Link>
-          {
-            pages.map(page => (
-              <Link to={page.path}>
-                <li className="whitespace-nowrap pl-3 pr-3 py-3" key={page.videoId}>
-                  {page.linkText}
-                </li>
-              </Link>
-            ))
-          }
+          <Grid
+            container
+            direction="column"
+            justify="center"
+            alignItems="center"
+            className="p-2"
+          >
+            <Link to="/">
+              <Button contained primary className="whitespace-nowrap pl-3 pr-3 py-3">
+                Home
+              </Button>
+            </Link>
+            {components.map(page =>
+              <NavigationLink key={page.videoId} page={page} />
+            )}
+          </Grid>
         </ul>
       </nav>
     </div>
---
+++ src/components/NavigationLink.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import { Link } from "react-router-dom";
+
+const NavigationLink = ({page}) => {
+  return (
+    <Link to={page.path}>
+      <Button contained primary className="whitespace-nowrap pl-3 pr-3 py-3" key={page.videoId}>
+        {page.linkText}
+      </Button>
+    </Link>
+  )
+}
+
+export default NavigationLink;
